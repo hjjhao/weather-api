c1d1cba16855744a291169f455c006bb2b1a8c7c
hjjhao <hjjhao@gmail.com>
2019-01-21 21:20:44 +1000

    other packages from homepc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = require('./router');
 const app = express();
+const helmet = require('helmet');
+const morgan = require('morgan');
+const notFound = require('./middleware/notFound');
 
-
+app.use(helmet());
+app.use(morgan('dev'));
 app.use(router);
+app.use(notFound);
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>console.log(`app listening at port ${port}`));
